fe3c92cdcea4ddc972ac7cd36556f9c5fc06a7b4
rayaraya <ryabinina.rb@phystech.edu>
2018-05-05 13:35:14 +0300

    add button locking

--- src/components/world_components/ui_components/ChapelView.js
+++ src/components/world_components/ui_components/ChapelView.js
@@ -5,14 +5,36 @@ class ChapelView extends Component {
     super(props);
     [this.command] = props.world.commands;
     this.command.execute = this.command.execute.bind(this.command);
+    this.state = {
+      canWakeUp: false,
+    };
+    this.lockButton = this.lockButton.bind(this);
   }
+
+  lockButton() {
+    this.setState(prevState => ({
+      canWakeUp: true,
+    }));
+  }
+
+  componentDidUpdate() {
+    if (this.state.canWakeUp === true) {
+      setInterval(() => {
+        this.setState(prevState => ({
+          canWakeUp: false,
+        }));
+      }, 5000);
+    }
+  }
+
   render() {
     return (<div>
         <div className="btn-group btn-group-lg">
-        <button type="button" className="btn btn-light" onClick={this.command.execute} >
+        <button id = "wake up the old man" type="button" className="btn btn-light" disabled={this.state.canWakeUp}
+        onClick={(event) => { this.command.execute(); this.lockButton(); }}>
                 {'wake up the old man'}
         </button>
-        <button type="button" className="btn btn-light" >
+        <button id = "look into window" type="button" className="btn btn-light" >
                 {'look into window'}
         </button>
         </div>
